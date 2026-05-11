26889db8b753ff68e03051aa3a6320a52f07258a
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-05-11 12:04:41 +0200

    rajout jobs04

---
+++ jour01/jobs04/jobs04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "entrer un nombre : " << endl;
+    cin >> a ;
+    cout << "entrer un nombre : " << endl;
+    cin >> b ;
+    cout << "le resulta de l'addition de " << a << " et " << b << " est : " << a + b << endl;
+}
