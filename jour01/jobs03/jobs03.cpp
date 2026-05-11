d35cf7b88e5e92f10a339f5de2fd458526a5f939
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-05-11 11:58:05 +0200

    rajout jobs03

---
+++ jour01/jobs03/jobs03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"entrer un nombre : " << endl;
+    int n;
+    cin >> n ;
+    while (n>0)
+    {
+        cout << "Hello World!" << endl;
+        n--;
+    }
+    return 0;
+}
