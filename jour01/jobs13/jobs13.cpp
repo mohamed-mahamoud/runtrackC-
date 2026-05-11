1f44f80137ab22ffc485fa191674378070943eb9
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-05-11 13:11:53 +0200

    rajout de la somme pour le jobs 13

--- jour01/jobs13/jobs13.cpp
+++ jour01/jobs13/jobs13.cpp
@@ -6,9 +6,12 @@ int main()
     int N;
     cout << "Entrez un nombre : ";
     cin >> N;
+    int somme = 0;
     for (int i =5; i <= N; i ++)
     {
+        somme+=somme+i*i*i;
         cout <<"la valeur au cube de " << i << " est : " << i * i * i << endl;
     }
+    cout << "La somme des cubes est : " << somme << endl;
     return 0;
 }
