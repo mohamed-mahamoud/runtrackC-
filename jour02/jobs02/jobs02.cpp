f69a9bbc9369a4480205b9fb3e6be55063657fb7
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-05-12 11:00:39 +0200

    jobs 02 fait

---
+++ jour02/jobs02/jobs02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    switch(n)
+    {
+        case 0: cout << "Nul\n";
+        case 1 :
+        case 2 : cout << "Petit\n"; 
+                        break;
+        case 3:
+        case 4:
+        case 5: cout << "Moyen\n"; 
+                        break;
+        default : cout << "Grand\n";
+    }
+}
+//n = 0 => Nul,Petit
+//n = 1 => Petit
+//n=4 => Moyen
+//n=10 => Grand
+//n=5 => Moyen
